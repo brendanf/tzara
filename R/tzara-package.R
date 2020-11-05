0e93091c0f97c211fe44c59f9954036d217aaf44
brendanf <brendan.furneaux@gmail.com>
2020-11-05 22:45:11 +0100

    add sample data to use in demo

---
+++ R/tzara-package.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' Sample amplicon sequences
+#'
+#' These are the three most common ASVs from Furneaux et al. 2020 (submitted).
+#' They are derived from PacBio RSII metabarcoding of soil samples from
+#' central Benin, West Africa.
+#' The three sequences are from the fungal genera _Penicillium_, _Agaricus_
+#' (used as a control sample), and _Inocybe_.
+#'
+#' @format A \code{\link[Biostrings]{DNAStringSet}} with three sequences.
+"tzara_sample"
