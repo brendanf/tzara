17326e818bdca214a5bbf9991d6e9ac62cba7dcd
brendanf <brendan.furneaux@gmail.com>
2020-11-05 23:36:47 +0100

    add sample quality score data

--- R/tzara-package.R
+++ R/tzara-package.R
@@ -9,11 +9,23 @@ NULL
 
 #' Sample amplicon sequences
 #'
-#' These are the three most common ASVs from Furneaux et al. 2020 (submitted).
+#' These are the three most abundant ASVs from
+#' \href{https://www.authorea.com/doi/full/10.22541/au.160340221.18389016/v1}{Furneaux et al. 2020}.
 #' They are derived from PacBio RSII metabarcoding of soil samples from
 #' central Benin, West Africa.
-#' The three sequences are from the fungal genera _Penicillium_, _Agaricus_
-#' (used as a control sample), and _Inocybe_.
+#' The three sequences are from the fungal genera \emph{Penicillium}, \emph{Agaricus}
+#' (included as a control), and \emph{Inocybe}.
 #'
 #' @format A \code{\link[Biostrings]{DNAStringSet}} with three sequences.
 "tzara_sample"
+
+#' Sample quality scores
+#'
+#' These are based on CCS quality scores from the first 1000 reads of long
+#' amplicon PacBio RSII samples in
+#' \href{https://www.authorea.com/doi/full/10.22541/au.160340221.18389016/v1}{Furneaux et al. 2020}.
+#'
+#' @format A \code{data.frame} with columns \code{seq_id}, \code{quality},
+#' and \code{n}, giving the number of times each wuality score appears in each
+#' read.
+"quality_profile"
